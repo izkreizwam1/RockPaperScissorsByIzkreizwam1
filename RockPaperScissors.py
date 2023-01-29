0d7d592a8abe162969e8446f5f9892c3ff873845
izkreizwam1 <tl_vasilev@yahoo.com>
2023-01-29 12:27:16 +0200

    Update RockPaperScissors.py
    add some lines

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,11 +1,14 @@
 import random
-
+from colored import fg
+# color = fg('red')
+# print(color+'aaaaaaaaaaaaaa')
+# print("\033[1;32m This text is Bright Green  \n")
 h=input('chose [r]ock , [p]aper, [s]cissors: ')
 c=random.choice(['r','p','s'])
 
 if h not in ['r','p','s']:
     raise SystemExit('invalid input, try again')
 print(f'computer choose: {c}')
-if h=='r' and c=='s' or h=='s' and c=='p' or h=='p' and c=='r': print('you win')
-elif h==c: print('draw')
-else: print('you lose')
+if h=='r' and c=='s' or h=='s' and c=='p' or h=='p' and c=='r': print(color + 'you win!!!',)
+elif h==c: print(color + 'draw!!')
+else: print(color + 'you lose!')
